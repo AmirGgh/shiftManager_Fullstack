f4a0edadb8b065126fc1ff059e0148f80a291b34
Amir Gez <111357168+AmirGgh@users.noreply.github.com>
2023-03-21 15:10:18 +0200

    Update db.js

--- service/configs/db.js
+++ service/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = () => {
     mongoose
-        .connect('mongodb://localhost:27017/factoryDB')
+        .connect(''mongodb+srv://gamiDB:Yonky5qiV73GRKSA@cluster0.i3tyd7m.mongodb.net/?retryWrites=true&w=majority')
         .then(() => console.log('Connected to factoryDB!'))
         .catch((error) => console.log(error));
 };
